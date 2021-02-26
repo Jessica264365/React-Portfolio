ce0c2dfdbc79eb560a11e04993d1e43d190108b9
Jessica Whitman <oprah135@gmail.com>
2021-02-26 11:50:30 -0800

    Created empty about me page for initial app setup

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Aboutme from "./pages/Aboutme";
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
+      <Route exact path="/" component={Aboutme} />
       <Footer />
-    </>
+    </Router>
   );
 }
 
---
+++ src/pages/Aboutme.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutMe() {
+  return (
+    <main className="container">
+      <section className="row">
+        <div className="col-lg-8"></div>
+      </section>
+    </main>
+  );
+}
+export default About;
