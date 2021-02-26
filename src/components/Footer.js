cb0a9c6dde378fcefc2ee1362108d64972e0eb1b
Jessica Whitman <oprah135@gmail.com>
2021-02-26 11:45:37 -0800

    Added Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footerline"></div>
+
+      <p className="text-muted footertext text-center">
+        &copy; Jessica Whitman 2021
+      </p>
+    </footer>
+  );
+}
+export default Footer;
