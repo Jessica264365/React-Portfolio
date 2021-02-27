03821d0a15fba534e6f85fbe8584eafa263fafdf
Jessica Whitman <oprah135@gmail.com>
2021-02-27 14:28:14 -0800

    Updating routes

--- src/App.js
+++ src/App.js
@@ -7,20 +7,12 @@ import Contact from "./pages/Contact/Contact";
 import Portfolio from "./pages/Portfolio/Portfolio";
 function App() {
   return (
-    <Router>
+    <Router basename="/React-Portfolio" >
       <NavTop />
 
       <Route exact path={process.env.PUBLIC_URL + "/"} component={Aboutme} />
-      <Route
-        exact
-        path={process.env.PUBLIC_URL + "/contact"}
-        component={Contact}
-      />
-      <Route
-        exact
-        path={process.env.PUBLIC_URL + "/portfolio"}
-        component={Portfolio}
-      />
+      <Route exact path={process.env.PUBLIC_URL + "/contact"} component={Contact} />
+      <Route exact path={process.env.PUBLIC_URL + "/portfolio"} component={Portfolio} />
 
       <Footer />
     </Router>
