1ec3c66e77b12b2f85437107110b2dc229e7d6a7
Jessica Whitman <oprah135@gmail.com>
2021-02-27 12:43:00 -0800

    Added switch to routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavTop from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer";
 import Aboutme from "./pages/Aboutme/Aboutme";
@@ -9,9 +9,11 @@ function App() {
   return (
     <Router basename={process.env.PUBLIC_URL}>
       <NavTop />
-      <Route exact path="/" component={Aboutme} />
-      <Route exact path="/contact" component={Contact} />
-      <Route exact path="/portfolio" component={Portfolio} />
+      <Switch>
+        <Route exact path="/" component={Aboutme} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/portfolio" component={Portfolio} />
+      </Switch>
       <Footer />
     </Router>
   );
